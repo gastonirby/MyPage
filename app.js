7d233380f1718d1e8c32f555e718ad6654c8f966
Gaston <gaston.irby@gmail.com>
2023-03-22 12:04:42 -0700

    added animation responsiveness

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const title = document.querySelector('h1')
 const btn = document.querySelectorAll('a')
 
+let mm = gsap.matchMedia()
+
 window.addEventListener('load', loader)
 
 function loader () {
@@ -10,28 +12,33 @@ function loader () {
     }
   })
 
-  TLLOAD.to('.images-container', { height: 400, duration: 1.3, delay: 0.4 })
-    // .to('.bloc-txt', {height: 'auto', duration: 0.6}, '-=0.8')
-    // .to('.bloc-txt h2', {y: 0, duration: 0.6}, '-=0.6 ')
-    .to('.f2', { y: 0, duration: 0.6 })
-    .to('.flip-img1', { display: 'none', duration: 0 })
-    .to('.f2', { y: '-100%', duration: 0.6 })
-    .to('.load-container', { autoAlpha: 0, duration: 2, delay: 1 })
-    .from('.name', { autoAlpha: 0, y: -500, duration: 5, ease: 'power4', 
-    }, '-=1')
-    .from('.aboutme', {
-      duration: 1.5,
-      autoAlpha: 0,
-      y: 200,
-      scale: 1,
-      scrollTrigger: {
-        trigger: '.aboutme',
-        start: 'top 75%',
-        end: 'bottom 70%',
-        scrub: 5
-      }
-    })
-    .from('.projects', {
+  mm.add('(min-width: 501px)', () => {
+    TLLOAD.to('.images-container', { height: 400, duration: 1.3, delay: 0.4 })
+      // .to('.bloc-txt', {height: 'auto', duration: 0.6}, '-=0.8')
+      // .to('.bloc-txt h2', {y: 0, duration: 0.6}, '-=0.6 ')
+      .to('.f2', { y: 0, duration: 0.6 })
+      .to('.flip-img1', { display: 'none', duration: 0 })
+      .to('.f2', { y: '-100%', duration: 0.6 })
+      .to('.load-container', { autoAlpha: 0, duration: 2, delay: 1 })
+      .from(
+        '.name',
+        { autoAlpha: 0, y: -500, duration: 5, ease: 'power4' },
+        '-=1'
+      )
+      .from('.aboutme', {
+        duration: 1.5,
+        autoAlpha: 0,
+        y: 200,
+        scale: 1,
+        scrollTrigger: {
+          trigger: '.aboutme',
+          start: 'top 75%',
+          end: 'bottom 70%',
+          scrub: 5,
+          markers: true
+        }
+      })
+      .from('.projects', {
         duration: 1.5,
         autoAlpha: 0,
         y: 200,
@@ -42,7 +49,70 @@ function loader () {
           end: 'bottom 80%',
           scrub: 5
         }
-    })
-} 
+      })
+  })
+}
 
 gsap.from('.card', { autoAlpha: 0, scale: 1.5, duration: 3 })
+
+mm.add('(max-width: 500px)', () => {
+  gsap.from('.name', { 
+    autoAlpha: 0, 
+    y: -500, 
+    duration: 2, 
+    ease: 'power4'
+  })
+  gsap.from('.aboutme', {
+    duration: 1.5,
+    autoAlpha: 0,
+    y: 200,
+    scale: 1,
+    scrollTrigger: {
+      trigger: '.aboutme',
+      start: 'top 75%',
+      end: 'bottom 90%',
+      scrub: 2,
+    }
+  })
+  gsap.from('.projects', {
+    duration: 1.5,
+    autoAlpha: 0,
+    y: 200,
+    scale: 1,
+    scrollTrigger: {
+      trigger: '.projects',
+      start: 'top 85%',
+      end: 'top 90%',
+      scrub: 3,
+      // markers: true
+    }
+  })
+})
+
+// mm.add('(max-width: 400px)', () => {
+//   gsap.from('.aboutme', {
+//     duration: 1,
+//     autoAlpha: 0,
+//     y: 200,
+//     scale: 1,
+//     scrollTrigger: {
+//       trigger: '.aboutme',
+//       start: 'top 80%',
+//       end: 'bottom 95%',
+//       scrub: 2,
+//       markers: true
+//     }
+//   })
+//   gsap.from('.projects', {
+//     duration: 1.5,
+//     autoAlpha: 0,
+//     y: 200,
+//     scale: 1,
+//     scrollTrigger: {
+//       trigger: '.projects',
+//       start: 'top 80%',
+//       end: 'bottom 95%',
+//       scrub: 2,
+//     }
+//   })
+// })
